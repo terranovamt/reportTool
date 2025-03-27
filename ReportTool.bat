9cf52b2f9df5abe260a2e94ad89d10a5fbcfec9d
Matteo Trranova <terranovamtt@gmail.com>
2025-03-27 10:23:53 +0100

    add exe of script

---
+++ ReportTool.bat
@@ -0,0 +1,37 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFA9RXhaDMmS3OpET/+b34OuDsXEaRuMvcLDZz6eHLuUA+XrgfJcqxHtXp9kJHBRKai24eg4goH1Wv2eWCMaSsgP4XkGK9XQ5GGp6uDGC2nlqNZ1hmcxj
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZko0
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQICLQtbXhC2M2S2Zg==
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWGqw+SI=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE2UMkPB5bDDCHLnm7CLoK6Yg=
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFA9RXhaDMmS3OpoS7czp5vyC4gBdZ9YScYzJ1bGCJeMH+ErqfJU+6kpIl8EJDw9kXxavIyA6lXtMsmGXd8OZpTPzT1iG9F8EFGp4u1HVnCQpdOdnlc9N1ji7nA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+python "C:\Program Files\Python\Project\ReportTool\main.py"
+
+echo Script completed. Press any key to close...
+pause >nul
